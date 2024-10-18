a08631e279d5aef0fe9074fbdd2b4debc624951b
RishiG2506 <clasherrishi@gmail.com>
2024-10-18 18:39:34 +0530

    Change in downsampling logic and running metrics

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 import requests
 import sqlite3
 import psycopg2
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
 import logging
 from typing import List, Dict
@@ -11,12 +11,24 @@ logger = logging.getLogger(__name__)
 
 
 
-
 class DataCollector:
     def __init__(self):
         self.symbols = ["BTCUSDT", "ETHUSDT"]
-        self.sampling_frequency = 2 #wait time in seconds
+        self.sampling_frequency = 5 #wait time in seconds
         self.base_url = "https://api.binance.com/api/v3/ticker/price"
+
+        self.running_metrics = {
+            symbol: {
+                'latest_price': None,
+                'high_price': None,
+                'low_price': None,
+                'open_price': None,
+                'latest_timestamp': None,
+                'avg_price': 0,
+                'sample_count': 0
+            } for symbol in self.symbols
+        }
+
         self.postgres_conn = psycopg2.connect(
             dbname="crypto_data",
             user="rg2506",
@@ -28,9 +40,8 @@ class DataCollector:
 
         self.sqlite_conn = sqlite3.connect('raw_data.db')
         self.setup_sqlite_db()
-        
-        self.running_metrics_conn = sqlite3.connect('running_metrics.db')
-        self.setup_running_metric_db()
+
+
 
     def setup_sqlite_db(self):
         """Initialize SQLite database for raw data"""
@@ -44,45 +55,44 @@ class DataCollector:
         ''')
         self.sqlite_conn.commit()
     
-    def setup_running_metric_db(self):
-        """Initialise SQLite database for running metrics (updated after every fetch)"""
-        cursor = self.running_metrics_conn.cursor()
-        
-        # Create running metrics table
-        cursor.execute('''
-            CREATE TABLE IF NOT EXISTS running_metrics (
-                symbol TEXT PRIMARY KEY,
-                latest_price REAL,
-                latest_timestamp TEXT,
-                daily_high REAL,
-                daily_low REAL,
-                processing_status TEXT
-            )
-        ''')
-
-        for symbol in self.symbols:
-            cursor.execute('''
-                INSERT OR IGNORE INTO running_metrics 
-                (symbol, processing_status) 
-                VALUES (?, 'initialized')
-            ''', (symbol,))
-            
-        self.running_metrics_conn.commit()
     
     def setup_postgres(self):
         cursor = self.postgres_conn.cursor()
+
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS downsampled_prices (
                 date DATE,
+                hour INTEGER,
                 symbol TEXT,
                 open_price REAL,
                 high_price REAL,
                 low_price REAL,
                 close_price REAL,
-                avg_price REAL
+                avg_price REAL,
+                sample_count INTEGER,
+                PRIMARY KEY (date, hour, symbol)
             )
         ''')
+
         self.postgres_conn.commit()
+    
+
+    def update_running_metrics(self, symbol, price, timestamp):
+        metrics = self.running_metrics[symbol]
+
+        if not metrics['open_price']:
+            metrics['open_price'] = price
+
+        metrics['latest_price'] = price
+        metrics['high_price'] = max(price, metrics['high_price']) if metrics['high_price'] is not None else price
+        metrics['low_price'] = min(price, metrics['low_price']) if metrics['low_price'] is not None else price
+        metrics['latest_timestamp'] = timestamp
+
+        total_sum = metrics['avg_price']*metrics['sample_count']
+        total_sum += price
+        metrics['sample_count'] += 1
+        metrics['avg_price'] = total_sum/metrics['sample_count']
+
     
     def fetch_prices(self) -> List[Dict]:
         """Fetch current prices for all symbols"""
@@ -92,14 +102,20 @@ class DataCollector:
                 response = requests.get(f"{self.base_url}?symbol={symbol}")
                 response.raise_for_status()
                 data = response.json()
+                price = float(data['price'])
+                timestamp = datetime.now()
+
+                self.update_running_metrics(symbol, price, timestamp)
+
                 results.append({
-                    'timestamp': datetime.now().isoformat(),
+                    'timestamp': timestamp,
                     'symbol': symbol,
-                    'price': float(data['price'])
+                    'price': price
                 })
             except Exception as e:
                 logger.error(f"Error fetching price for {symbol}: {str(e)}")
         return results
+    
     
     def store_raw_data(self, raw_data: List[Dict]):
         """Store raw cryto data in SQLite"""
@@ -110,118 +126,132 @@ class DataCollector:
                 (entry['timestamp'], entry['symbol'], entry['price'])
             )
         self.sqlite_conn.commit()
-
     
-    def downsample_and_store(self):
-        """Calculate EOD stats for each symbol and store in Postgres"""
+    def clear_raw_data(self):
+        """Clear raw data from SQLite at EOD"""
         cursor = self.sqlite_conn.cursor()
-        pg_cursor = self.postgres_conn.cursor()
-        
-        for symbol in self.symbols:
-            # Get EOD stats from raw data (sqlite)
-            cursor.execute('''
-                SELECT 
-                    date(timestamp) as date,
-                    MIN(price) as low_price,
-                    MAX(price) as high_price,
-                    AVG(price) as avg_price
-                FROM raw_prices 
-                WHERE symbol = ?
-                GROUP BY date(timestamp)
-            ''', (symbol,))
-            eod_stats = cursor.fetchone()
+        cursor.execute("DELETE FROM raw_prices")
+        self.sqlite_conn.commit()
 
-            cursor.execute('''
-                SELECT 
-                    price
-                FROM raw_prices 
-                WHERE symbol = ?
-                ORDER BY timestamp
-                LIMIT 1
-            ''', (symbol,))
-            opening_price = cursor.fetchone()
+        logger.info("Cleared Raw Price Data")
 
-            cursor.execute('''
-                SELECT 
-                    price
-                FROM raw_prices 
-                WHERE symbol = ? 
-                ORDER BY timestamp DESC
-                LIMIT 1
-            ''', (symbol,))
-            closing_price = cursor.fetchone()
-
-            print(symbol)
-            print(eod_stats)
-            print(opening_price)
-
-            
-            # Store downsampled_data in PostgreSQL store
-            pg_cursor.execute('''
-                        INSERT INTO downsampled_prices
-                        (date, symbol, open_price, high_price, low_price, close_price, avg_price)
-                        VALUES (%s, %s, %s, %s, %s, %s, %s)
-                              ''',
-                        (eod_stats[0], symbol, opening_price[0], eod_stats[2], eod_stats[1], closing_price[0], eod_stats[3]))
-            
-        
-        self.postgres_conn.commit()
     
-    def update_running_metrics(self, data: List[Dict]):
-        """Real-time monitoring of metrics"""
-        cursor = self.running_metrics_conn.cursor()
+
+    def store_downsampled_data(self, hour_datetime: datetime):
+        pg_cursor = self.postgres_conn.cursor()
+
+        for symbol, metrics in self.running_metrics.items():
+            if metrics['sample_count'] > 0:
+
+                pg_cursor.execute('''
+                    INSERT INTO downsampled_prices 
+                    (date, hour, symbol, open_price, high_price, low_price, 
+                     close_price, avg_price, sample_count)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                ''', (
+                    hour_datetime.date(),
+                    hour_datetime.hour,
+                    symbol,
+                    metrics['open_price'],
+                    metrics['high_price'],
+                    metrics['low_price'],
+                    metrics['latest_price'],
+                    metrics['avg_price'],
+                    metrics['sample_count']
+                ))
+        self.postgres_conn.commit()
+        logger.info(f"Stored Hourly Metrics for Hour {hour_datetime.hour}")
+
+
+    def reset_running_metrics(self):
+        """Reset running metrics for new hour"""
+        for metrics in self.running_metrics.values():
+            metrics['open_price'] = None
+            metrics['latest_price'] = None
+            metrics['high_price'] = None
+            metrics['low_price'] = None
+            metrics['avg_price'] = 0
+            metrics['sample_count'] = 0
+            metrics['latest_timestamp'] = None
+
+    
+    # def update_running_metrics(self, data: List[Dict]):
+    #     """Real-time monitoring of metrics"""
+    #     cursor = self.running_metrics_conn.cursor()
         
-        for entry in data:
-            symbol = entry['symbol']
-            current_price = entry['price']
-            current_timestamp = entry['timestamp']
+    #     for entry in data:
+    #         symbol = entry['symbol']
+    #         current_price = entry['price']
+    #         current_timestamp = entry['timestamp']
             
             
-            # Update all metrics
-            cursor.execute('''
-                UPDATE running_metrics 
-                SET 
-                    latest_price = ?,
-                    latest_timestamp = ?,
-                    daily_high = MAX(?, COALESCE(daily_high,?)),
-                    daily_low = MIN(?, COALESCE(daily_low, ?)),
-                    processing_status = 'active'
-                WHERE symbol = ?
-            ''', (
-                current_price,
-                current_timestamp,
-                current_price, current_price,
-                current_price, current_price,
-                symbol
-            ))
+    #         # Update all metrics
+    #         cursor.execute('''
+    #             UPDATE running_metrics 
+    #             SET 
+    #                 latest_price = ?,
+    #                 latest_timestamp = ?,
+    #                 daily_high = MAX(?, COALESCE(daily_high,?)),
+    #                 daily_low = MIN(?, COALESCE(daily_low, ?)),
+    #                 processing_status = 'active'
+    #             WHERE symbol = ?
+    #         ''', (
+    #             current_price,
+    #             current_timestamp,
+    #             current_price, current_price,
+    #             current_price, current_price,
+    #             symbol
+    #         ))
         
-        self.running_metrics_conn.commit()
+    #     self.running_metrics_conn.commit()
 
     
     def run(self):
         try:
-            #2 iterations for testing purposes. To be inside while True:
-            for i in range(2):
+            current_hour = datetime.now().hour
+            while True:
+
+                current_time = datetime.now()
+                
+                if current_time.hour != current_hour:
+                    previous_hour = current_time - timedelta(hours=1)
+                    previous_hour = previous_hour.replace(minute=0, second=0, microsecond=0)
+                    self.store_downsampled_data(previous_hour)
+                    self.reset_running_metrics()
+                    current_hour = current_time.hour
+
+                if current_time.hour == 0:
+                    self.clear_raw_data()
+
+
+
                 # Fetch and store prices
                 current_prices = self.fetch_prices()
-                self.update_running_metrics(current_prices)
                 self.store_raw_data(current_prices)
                 
                 # Log current prices
                 for price_data in current_prices:
                     logger.info(f"{price_data['symbol']}: ${price_data['price']:.2f} at {price_data['timestamp']}")
                 
+                for symbol in self.symbols:
+                        metrics = self.running_metrics[symbol]
+                        logger.info(
+                            f"{symbol} - Hour {current_hour}: "
+                            f"Current: ${metrics['latest_price']}, "
+                            f"High: ${metrics['high_price']}, "
+                            f"Low: ${metrics['low_price']}, "
+                            f"Avg: ${metrics['avg_price']}, "
+                            f"Samples: {metrics['sample_count']}"
+                        )
+                
 
                 time.sleep(self.sampling_frequency)
-            
-            self.downsample_and_store()
-                
+
         except KeyboardInterrupt:
             logger.info("Stopping Data Ingestion...")
         finally:
             self.postgres_conn.close()
             self.sqlite_conn.close()
-            self.running_metrics_conn.close()
 
 
 if __name__ == "__main__":
